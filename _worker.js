f39df1cd51d738133a16ef12849b8e92eb8bba2f
zsdbbn <zsdbbn@outlook.com>
2024-01-05 20:31:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,8 +6,8 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const proxyIPs = ['[2404:c140:1f00:8:0:1:0:f1]'];
-
+//const proxyIPs = ['[2404:c140:1f00:8:0:1:0:f1]'];
+const proxyIPs = ['23.162.136.169'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
